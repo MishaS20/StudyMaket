85dab33f8a3b6e12c1f536e7f0b1d6cb6ea016ea
MishaS20 <selyavin.misha@mail.ru>
2021-03-17 20:40:21 +0600

    Vertical slider

--- js/script.js
+++ js/script.js
@@ -5,4 +5,31 @@ $(document).ready(function () {
     $('.services__item-title').click(function (event) {
         $(this).toggleClass('active').next().slideToggle(300);
     });
+    $(".slider").slick({
+        vertical: true,
+        arrows: true,
+        verticalSwiping: true,
+        slidesToShow: 2,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        swipe: true,
+        waitForAnimate: false,
+        // pauseOnDotsHover: true,
+    });
+    // $('.slick-vertical').slick({
+    //     vertical: true,
+    //     verticalSwiping: true,
+    //     slidesToShow: 2,
+    //     autoplay: true,
+    // });
+    // $(function () {
+    //     $('.slider').slick({
+    //         vertical: true,
+    //         verticalSwiping: true,
+    //         slidesToShow: 2,
+    //         autoplay: true,
+    //     });
+    // });
 })
